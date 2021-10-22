09c89910cb4a48edf9661ace4f15a4f715be0c0a
Justinian074 <jul074@ucsd.edu>
2021-10-22 00:27:42 -0700

    part 2 done

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,62 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  var synth = window.speechSynthesis;
+  var voiceSelect = document.getElementById('voice-select');
+  var voices = [];
+
+  const button = document.querySelector('button');
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+
+    for(var i = 0; i < voices.length ; i++) {
+      var option = document.createElement('option');
+      option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+
+      if(voices[i].default) {
+        option.textContent += ' -- DEFAULT';
+      }
+
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  button.addEventListener('click', event => {
+    const imgFace = document.querySelector('img[alt="Smiling face"]');
+    var theText = document.getElementById("text-to-speak").value;
+    console.log(theText);
+
+    var utterThis = new SpeechSynthesisUtterance(theText);
+    var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+
+    for(var i = 0; i < voices.length ; i++) {
+      if(voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+  
+    //imgFace.src = 'assets/images/smiling-open.png';
+    synth.speak(utterThis);
+    //imgFace.src = 'assets/images/smiling.png';
+    var timer = setInterval(
+      function(){ 
+        console.log("hi");
+        if(synth.speaking == true){
+          imgFace.src = 'assets/images/smiling-open.png'; 
+        }
+        else{
+          imgFace.src = 'assets/images/smiling.png'
+          clearInterval(timer);
+        }
+      }
+    , 10);
+  });
 }
